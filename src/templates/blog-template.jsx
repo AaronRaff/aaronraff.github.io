cb48d33f1d7ae141f2523dfed555868866222b01
Aaron Raff <aaronraffdev@gmail.com>
2020-05-20 21:32:28 -0400

    add share buttons to blog posts

--- src/templates/blog-template.jsx
+++ src/templates/blog-template.jsx
@@ -3,11 +3,37 @@ import { graphql } from "gatsby";
 import PropTypes from "prop-types";
 import Img from "gatsby-image";
 import { DiscussionEmbed } from "disqus-react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTwitter, faFacebook } from "@fortawesome/free-brands-svg-icons";
 import Layout from "../components/layout";
 
 export default function Template({ data }) {
   const { markdownRemark } = data;
   const { frontmatter, html } = markdownRemark;
+  const pageLink = encodeURIComponent(
+    `https://aaronraff.dev${frontmatter.slug}`
+  );
+  const links = [
+    `https://twitter.com/intent/tweet?url=${pageLink}`,
+    `http://www.facebook.com/share.php?u=${pageLink}`,
+  ];
+  const icons = [
+    <FontAwesomeIcon icon={faTwitter} />,
+    <FontAwesomeIcon icon={faFacebook} />,
+  ];
+
+  const shareLinks = icons.map((icon, index) => {
+    return (
+      <a
+        href={links[index]}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="inline-block mx-4 text-lg"
+      >
+        {icon}
+      </a>
+    );
+  });
 
   return (
     <Layout>
@@ -22,8 +48,15 @@ export default function Template({ data }) {
       <p className="font-light text-center text-sm text-gray-500 mt-2">
         {frontmatter.photoCreds}
       </p>
-      <div className="post mt-4 mb-16">
+      <div className="post mt-4">
         <div dangerouslySetInnerHTML={{ __html: html }} />
+      </div>
+      <div className="text-center my-16 font-light">
+        <p>
+          If you liked this post, it would mean a lot to me if you shared it
+          with your friends!
+        </p>
+        <div className="mt-4 text-gray-500">{shareLinks}</div>
       </div>
       <DiscussionEmbed
         shortname={process.env.GATSBY_DISQUS_NAME}
