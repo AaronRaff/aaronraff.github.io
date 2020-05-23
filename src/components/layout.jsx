315d2c117c46ebb4eb59b300c4cbf51d16454ae0
Aaron Raff <aaronraffdev@gmail.com>
2020-05-23 18:24:22 -0400

    fix bug where fontawesome icon loads in way too big

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
 import Header from "./header";
 import Footer from "./footer";
 
 export default function Layout({ children }) {
+  config.autoAddCss = false;
   return (
     <div className="font-body my-16 mx-8 lg:my-16 lg:mx-40">
       <Header />
