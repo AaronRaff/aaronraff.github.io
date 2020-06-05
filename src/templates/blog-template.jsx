6d1db54c1b8012cd8641ee03858fbd7ba3209858
Aaron Raff <aaronraffdev@gmail.com>
2020-06-05 11:51:23 -0400

    stop uri encoding social share links

--- src/templates/blog-template.jsx
+++ src/templates/blog-template.jsx
@@ -12,9 +12,7 @@ import SEO from "../components/seo";
 export default function Template({ data }) {
   const { markdownRemark } = data;
   const { frontmatter, html } = markdownRemark;
-  const pageLink = encodeURIComponent(
-    `https://www.aaronraff.dev${frontmatter.slug}`
-  );
+  const pageLink = `https://www.aaronraff.dev${frontmatter.slug}`;
   const links = [
     `https://twitter.com/intent/tweet?url=${pageLink}`,
     `http://www.facebook.com/share.php?u=${pageLink}`,
