1998bfe02b6683756b18e4302b32b5a70a3c161d
Aaron Raff <aaronraffdev@gmail.com>
2020-06-06 23:23:03 -0400

    add mailto link to footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,20 +2,23 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faGithub,
-  faTwitter,
   faLinkedin,
+  faTwitter,
 } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 export default function Footer() {
   const links = [
     "https://github.com/aaronraff",
     "https://twitter.com/aaronraff_",
     "https://linkedin.com/in/aaronraff",
+    "mailto:aaronraffdev@gmail.com",
   ];
   const icons = [
     <FontAwesomeIcon icon={faGithub} />,
     <FontAwesomeIcon icon={faTwitter} />,
     <FontAwesomeIcon icon={faLinkedin} />,
+    <FontAwesomeIcon icon={faEnvelope} />,
   ];
 
   const elts = icons.map((icon, index) => {
